aa896d9503274de4882f635ee43a0b169e5fef5d
João Fidalgo <joao.fidalgo.1403@tecnico.ulisboa.pt>
2024-03-03 22:55:32 +0000

    F2.1.6 Methods and parameters added to Participation related classes

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/domain/Participation.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/domain/Participation.java
@@ -1,19 +1,16 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain;
 
 import jakarta.persistence.*;
-import org.springframework.security.crypto.keygen.KeyGenerators;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.User;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Member;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
 
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.PARTICIPATION_BEFORE_APPLICATION_END;
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.PARTICIPATION_DUPLICATE;
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.PARTICIPATION_EXCEEDS_ACTIVITY_LIMIT;
+
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
 
@@ -30,7 +27,7 @@ public class Participation {
     @Column(name = "acceptance_date")
     private LocalDateTime acceptanceDate;
 
-    @ManyToMany
+    @ManyToOne
     private Activity activity;
 
     @ManyToOne
@@ -93,20 +90,20 @@ public class Participation {
     }
 
     private void checkActivityLimit() {
-        if (activity.getParticipations().length() + 1 > activity.getParticipationLimit()) {
-            throw new IllegalArgumentException("Number of participations exceeded");
+        if (activity.getParticipations().size() + 1 > activity.getParticipantsNumberLimit()) {
+            throw new HEException(PARTICIPATION_EXCEEDS_ACTIVITY_LIMIT);
         }
     }
 
     private void checkDuplicatedParticipation() {
         if (activity.getParticipations().contains(this)) {
-            throw new IllegalArgumentException("Volunteer is already participating in this activity");
+            throw new HEException(PARTICIPATION_DUPLICATE);
         }
     }
 
     private void checkApplicationDeadline() {
         if (acceptanceDate.isBefore(activity.getApplicationDeadline())) {
-            throw new IllegalArgumentException("Application period is still ongoing");
+            throw new HEException(PARTICIPATION_BEFORE_APPLICATION_END);
         }
     }
 
--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/user/domain/Volunteer.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/user/domain/Volunteer.java
@@ -2,6 +2,7 @@ package pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain;
 
 import jakarta.persistence.*;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
 
 import java.util.ArrayList;
@@ -10,8 +11,11 @@ import java.util.List;
 @Entity
 @DiscriminatorValue(User.UserTypes.VOLUNTEER)
 public class Volunteer extends User {
-    public Volunteer() {
-    }
+        //CHECK IN CLASS
+        @OneToMany(cascade = CascadeType.ALL, mappedBy = "activity", orphanRemoval = true, fetch = FetchType.EAGER)
+        private List<Participation> participations = new ArrayList<>();
+        public Volunteer() {
+        }
 
     public Volunteer(String name, String username, String email, AuthUser.Type type, State state) {
         super(name, username, email, Role.VOLUNTEER, type, state);
@@ -20,4 +24,14 @@ public class Volunteer extends User {
     public Volunteer(String name, State state) {
         super(name, Role.VOLUNTEER, state);
     }
+
+
+    public List<Participation> getParticipations() {
+        return this.participations;
+    }
+
+    public void addParticipation(Participation participation) {
+        this.participations.add(participation);
+    }
+
 }
