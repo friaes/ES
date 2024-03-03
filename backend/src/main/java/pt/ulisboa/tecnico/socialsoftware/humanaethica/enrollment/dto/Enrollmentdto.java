f61c9acbebdc80a7055e33b74d7efd43895bfec0
Frederico Soares <frederico.soares@tecnico.ulisboa.pt>
2024-03-03 10:47:04 +0000

    feat: java dto for Enrollment implemented

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/Enrollmentdto.java
@@ -0,0 +1,77 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto;
+
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.dto.UserDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto;
+
+public class Enrollmentdto {
+    
+    private Integer id;
+    private String motivation;
+    private String enrollmentDate;
+    private UserDto volunteer;
+    private ActivityDto activity;
+
+    public Enrollmentdto() {
+    }
+
+    public Enrollmentdto(Enrollment enrollment) {
+        setId(enrollment.getId());
+        setMotivation(enrollment.getMotivation());
+        setEnrollmentDate(DateHandler.toISOString(enrollment.getEnrollmentDate()));
+        setVolunteer(new UserDto(enrollment.getVolunteer()));
+        setActivity(new ActivityDto(enrollment.getActivity(), false));
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMotivation() {
+        return motivation;
+    }
+
+    public void setMotivation(String motivation) {
+        this.motivation = motivation;
+    }
+
+    public String getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(String enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public UserDto getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(UserDto volunteer) {
+        this.volunteer = volunteer;
+    }
+
+    public ActivityDto getActivity() {
+        return activity;
+    }
+
+    public void setActivity(ActivityDto activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public String toString() {
+        return "EnrollmentDto{" +
+                "id=" + id +
+                ", motivation='" + motivation + '\'' +
+                ", enrollmentDate='" + enrollmentDate + '\'' +
+                ", volunteer=" + volunteer +
+                ", activity=" + activity +
+                '}';
+    }
+}
