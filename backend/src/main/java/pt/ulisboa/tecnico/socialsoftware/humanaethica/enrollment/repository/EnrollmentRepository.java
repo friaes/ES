759190f09f2675f47fb660827ac48c1fc1b739f6
Josegron <josemarialuis@tecnico.ulisboa.pt>
2024-03-04 10:41:07 +0000

    Fix: Enrolment Repository

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/repository/EnrollmentRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/repository/EnrollmentRepository.java
@@ -1,40 +1,23 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
 
 import java.util.List;
 
-public class EnrollmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-    @PersistenceContext
-    private EntityManager entityManager;
+@Repository
+@Transactional
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer>{
 
-    public List<Enrollment> findEnrollmentsByVolunteer(Volunteer volunteer) {
-        return entityManager.createQuery("SELECT e FROM Enrollment e WHERE e.volunteer = :volunteer", Enrollment.class)
-                .setParameter("volunteer", volunteer)
-                .getResultList();
-    }
+    @Query("SELECT e FROM Enrollment e WHERE e.volunteer = :volunteer")
+    List<Enrollment> findEnrollmentsByVolunteer(Volunteer volunteer);
 
-    public List<Enrollment> findEnrollmentsByActivity(Activity activity) {
-        return entityManager.createQuery("SELECT e FROM Enrollment e WHERE e.activity = :activity", Enrollment.class)
-                .setParameter("activity", activity)
-                .getResultList();
-    }
-
-    public Enrollment save(Enrollment enrollment) {
-        entityManager.persist(enrollment);
-        return enrollment;
-    }
-
-    public Enrollment update(Enrollment enrollment) {
-        return entityManager.merge(enrollment);
-    }
-
-    public void delete(Enrollment enrollment) {
-        entityManager.remove(enrollment);
-    }
+    @Query("SELECT e FROM Enrollment e WHERE e.activity = :activity")
+    List<Enrollment> findEnrollmentsByActivity(Activity activity);
 }
