8393a00b2e3c1ca05786cfd9dfd7d8ff03dd66ec
Mikhail Shueb <mikhail.shueb@tecnico.ulisboa.pt>
2024-03-07 17:20:17 +0000

    feat: Added Assessment Controller

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
@@ -0,0 +1,31 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Member;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+public class AssessmentController {
+    @Autowired
+    private AssessmentService service;
+
+    @GetMapping("/assessment/{institutionId}")
+    public List<AssessmentDto> getAssessmentsByInstitution(@PathVariable int institutionId) {
+        return service.getAssessmentsByInstitution(institutionId);
+    }
+
+    @PostMapping("/assessment/register")
+    @PreAuthorize("hasRole('ROLE_VOLUNTEER')")
+    public AssessmentDto createAssessment(Integer userId, Integer institutionId, AssessmentDto assessmentDto) {
+        return service.createAssessment(userId, institutionId, assessmentDto);
+    }
+
+}
