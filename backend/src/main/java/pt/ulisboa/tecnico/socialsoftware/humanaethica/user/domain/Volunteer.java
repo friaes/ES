d945175cfae80f2d2f9bb47277fef866c783e5a2
Antonio Oliveira <antonio.koch@tecnico.ulisboa.pt>
2024-03-07 17:53:39 +0000

    fix: added volunteer.getAssessments

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/user/domain/Volunteer.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/user/domain/Volunteer.java
@@ -1,16 +1,24 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain.Assessment;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @DiscriminatorValue(User.UserTypes.VOLUNTEER)
 public class Volunteer extends User {
+    @OneToMany(mappedBy = "volunteer")
+    private List<Assessment> assessments;
+
     public Volunteer() {
+    }
+
+    public List<Assessment> getAssessments() {
+        return assessments;
     }
 
     public Volunteer(String name, String username, String email, AuthUser.Type type, State state) {
