c0f373021bfa762b5edd942eb0583f2fb18f5072
Mikhail Shueb <mikhail.shueb@tecnico.ulisboa.pt>
2024-03-07 17:28:10 +0000

    feat: Added Assessment Service

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentService.java
@@ -0,0 +1,51 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment;
+
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain.Assessment;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.repository.AssessmentRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.repository.InstitutionRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.repository.UserRepository;
+
+import java.util.List;
+
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.*;
+
+@Service
+public class AssessmentService {
+    @Autowired
+    AssessmentRepository ar;
+
+    @Autowired
+    UserRepository ur;
+
+    @Autowired
+    InstitutionRepository ir;
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public List<AssessmentDto> getAssessmentsByInstitution(Integer institutionId) {
+        List<Assessment> a = ar.getAssessments(institutionId);
+        return a.stream()
+                .map(AssessmentDto::new)
+                .toList();
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public AssessmentDto createAssessment(Integer userId, Integer institutionId, AssessmentDto assessmentDto) {
+        Institution i = ir.getReferenceById(institutionId);
+        Volunteer v = (Volunteer) ur.getReferenceById(userId);
+
+        Assessment a = new Assessment(i, v, assessmentDto);
+        ar.save(a);
+
+        return new AssessmentDto(a);
+    }
+}
