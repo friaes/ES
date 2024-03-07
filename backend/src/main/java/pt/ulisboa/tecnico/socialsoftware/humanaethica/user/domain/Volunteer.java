9fe0e02f5e53e1f553a7638e19940f9a2532dc45
Antonio Oliveira <antonio.koch@tecnico.ulisboa.pt>
2024-03-07 17:53:39 +0000

    fix: added volunteer.getAssessments

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/user/domain/Volunteer.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/user/domain/Volunteer.java
@@ -1,12 +1,13 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain;
 
 import jakarta.persistence.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain.Assessment;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @DiscriminatorValue(User.UserTypes.VOLUNTEER) 
@@ -19,7 +20,14 @@ public class Volunteer extends User {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "volunteer", orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Participation> participations = new ArrayList<>();
 
+    @OneToMany(mappedBy = "volunteer")
+    private List<Assessment> assessments;
+
     public Volunteer() {
+    }
+
+    public List<Assessment> getAssessments() {
+        return assessments;
     }
 
     public Volunteer(String name, String username, String email, AuthUser.Type type, State state) {
