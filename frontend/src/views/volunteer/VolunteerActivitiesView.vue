d1695bb0b3f94b00829826ef9fe45dc9b53ce251
Frederico Soares <frederico.soares@tecnico.ulisboa.pt>
2024-03-20 20:59:28 +0000

    feat: Implementação inicial em VolunteerActivitiesView do botão de Apply for Activity(Issue#51)

--- frontend/src/views/volunteer/VolunteerActivitiesView.vue
+++ frontend/src/views/volunteer/VolunteerActivitiesView.vue
@@ -40,6 +40,19 @@
             </template>
             <span>Report Activity</span>
           </v-tooltip>
+          <v-tooltip>
+            <template v-slot:activator="{ on }">
+              <v-icon
+                class="mr-2 action-button"
+                color="green"
+                v-on="on"
+                data-cy="applyButton"
+                @click="applyActivity(item)"
+                >mdi-check-bold</v-icon
+              >
+            </template>
+            <span>Apply for Activity</span>
+          </v-tooltip>
         </template>
       </v-data-table>
     </v-card>
