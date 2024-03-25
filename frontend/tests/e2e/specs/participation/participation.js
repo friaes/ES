cdfc0691326a56771a3d2cf7af8eed2c8da2399d
João Fidalgo <joao.fidalgo.1403@tecnico.ulisboa.pt>
2024-03-25 23:46:29 +0000

    test: Selecionar show enrollments da primeira atividade (#88)

--- frontend/tests/e2e/specs/participation/participation.js
+++ frontend/tests/e2e/specs/participation/participation.js
@@ -8,14 +8,16 @@ describe('Participation', () => {
 
         cy.demoMemberLogin();
         cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+        cy.intercept('GET', '/activities/*/enrollments').as('getEnrollments');
+        
         cy.get('[data-cy="institution"]').click();
         cy.get('[data-cy="activities"]').click();
         cy.wait('@getInstitutions');
         cy.get('[data-cy="memberActivitiesTable"] tbody tr')
         .should('have.length', 2)
         .eq(0).children().eq(3).should('contain', '1')
-        cy.logout();
-
+        cy.get('[data-cy="showEnrollments"]').eq(0).click()
+        cy.wait('@getEnrollments');
     });
 });
   
