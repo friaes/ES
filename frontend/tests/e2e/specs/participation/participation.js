8fea556d40b9646ba4f17e61e6c744f99bd73598
João Fidalgo <joao.fidalgo.1403@tecnico.ulisboa.pt>
2024-03-25 23:22:48 +0000

    test: Verifição do número de participações da primeira atividade (#84)

--- frontend/tests/e2e/specs/participation/participation.js
+++ frontend/tests/e2e/specs/participation/participation.js
@@ -5,16 +5,17 @@ describe('Participation', () => {
     });
 
     it('create participations', () => {
-        
-        
+
         cy.demoMemberLogin();
         cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
         cy.get('[data-cy="institution"]').click();
         cy.get('[data-cy="activities"]').click();
         cy.wait('@getInstitutions');
         cy.get('[data-cy="memberActivitiesTable"] tbody tr')
-        .should('have.length', 2) 
+        .should('have.length', 2)
+        .eq(0).children().eq(3).should('contain', '1')
         cy.logout();
 
     });
 });
+  
