43b8b82d03bf2c2d1ef7a50c40e87bb10a87233a
Frederico Soares <frederico.soares@tecnico.ulisboa.pt>
2024-03-26 17:15:38 +0000

    fix: change InstitutionActivitiesView collumns name

--- frontend/src/views/member/InstitutionActivitiesView.vue
+++ frontend/src/views/member/InstitutionActivitiesView.vue
@@ -106,7 +106,7 @@ export default class InstitutionActivitiesView extends Vue {
       width: '5%',
     },
     {
-      text: 'Number of Participants',
+      text: 'Number of Enrollments',
       value: 'numberOfEnrollments',
       align: 'left',
       width: '5%',
