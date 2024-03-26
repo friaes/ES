22c43e1b5db80323eb1a915fcf569d9c1ce76262
Antonio Oliveira <antonio.koch@tecnico.ulisboa.pt>
2024-03-26 16:04:31 +0000

    feat: (Issue: #65) implemented getVolunteerAssessments web service

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentService.java
@@ -52,4 +52,14 @@ public class AssessmentService {
 
         return new AssessmentDto(assessment);
     }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public List<AssessmentDto> getVolunteerAssessments(Integer userId) {
+        if (userId == null) throw  new HEException(USER_NOT_FOUND);
+        userRepository.findById(userId).orElseThrow(() -> new HEException(USER_NOT_FOUND, userId));
+
+        return assessmentRepository.getVolunteerAssessments(userId).stream()
+                .map(AssessmentDto::new)
+                .toList();
+    }
 }
